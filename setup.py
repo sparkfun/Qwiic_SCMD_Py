0be595655038b9f8865a0c6125751f1bb2e4576e
KDB <kirk.benell@sparkfun.com>
2019-06-25 14:14:52 -0600

    version fix

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='0.0.7',
+    version='0.0.9',
 
     description='SparkFun Electronics qwiic CCS811',
     long_description=long_description,
@@ -77,7 +77,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
- 
+
     ],
 
     # What does your project relate to?
